e90705a41ff1d8b033e46618b58831ed4d2f2fe4
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2016-11-01 22:01:07 +0100

    Level 2 refactored

--- level2.py
+++ level2.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python2
-from collections import Counter
 
-counter = Counter()
 with open("mess.txt",'r') as f:
-  mess = f.readlines()
-
-mess = "".join(mess) 
-mess = mess.replace("\n","")
-for c in map(None,mess):
-  counter[c] += 1
-
-# sort counter
-counter_rare_chars = counter.most_common()[-8:]
-rare_chars = {}
-for c in counter_rare_chars:
-  rare_chars[mess.index(c[0])] = c[0]
-
-rare_chars = sorted(rare_chars.items())
+  mess = f.read()
 result = ""
-for k,v in rare_chars:
- result += v
+counts = {}
+for c in mess:
+  if counts.has_key(c):
+    continue
+  counts[c] = mess.count(c)
+  if counts[c] < 10:
+    result += c
 
 print result
-
-
