c430101f78470ec0da0b1641d07d4e3b50151812
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2016-11-04 11:40:57 +0100

    refactoring: breaking at the result

--- level4.py
+++ level4.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python2
 
 import urllib
+import re
 
 def next_nothing(nothing):
   url = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=" + nothing
   f = urllib.urlopen(url)
   nothing = f.read()
-  nothing = nothing.split(" ")[-1] 
   return nothing
 
 nothing = "12345"
-for _ in range(400):
+while True:
   nothing = next_nothing(nothing)
-  print nothing
+  match =  re.match(r".*and the next nothing is (\d+)",nothing)
+  if match == None:
+    if "going" not in nothing:
+      print nothing
+      break
+  else: 
+    nothing = match.group(1)
